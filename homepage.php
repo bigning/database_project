1f2a3d1aa79f497d59e017f2cb9c7538a3ffca7e
bigning <bigning1118@gmail.com>
2016-12-03 17:05:03 -0500

    add session for homepage

--- homepage.php
+++ homepage.php
@@ -1,4 +1,16 @@
+<?php
+// return uesr_id, user_name, user_profile, user_icon
+//
+error_reporting(-1);
 
+session_start();
+$user_id = $_SESSION["user_name"];
+$user_name = $_SESSION["user_name"];
+$user_profile = $_SESSION["user_profile"];
+$user_icon = $_SESSION["user_icon"];
+
+echo $user_name;
+?>
 
 
 
