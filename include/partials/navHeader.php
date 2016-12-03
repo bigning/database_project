01a686f71637db2c5d596691a47b136337dd8842
BoogieJay <jw4100@nyu.edu>
2016-12-03 16:55:26 -0500

    change cover

--- include/partials/navHeader.php
+++ include/partials/navHeader.php
@@ -3,9 +3,17 @@
 <head>
 	<title>Cookzilla</title>
 	<link rel="stylesheet" type="text/css" href="./include/framework/bootstrap.min.css">
+
+	<style type="text/css">
+	.navbar .navbar-default{ 
+		margin: 0 0;
+		background: rgba(0,0,0,0.0);
+	}
+	</style>
+
 </head>
 <body>
-<nav class="navbar navbar-default navbar-inverse">
+<nav class="navbar navbar-default">
   	<div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
--- welcomePage.php
+++ welcomePage.php
@@ -10,23 +10,17 @@
 
 <?php require "./include/partials/navFooter.php" ?>
 
-
 <div>
 	<h1>Register</h1>
 	<form method = "POST" action = "./register.php">
-		<div class = "form-group">
-			<label for = "username">Username</label>
-			<input type="text" name="username" id = "username" required>
-			<label for = "password">Password</label>
-			<input type="password" name="password" id = "password" required>
-			<label for = "description">Description</label>
-			<textarea id = "description" name = "description"></textarea>
-			<label for = "icon">Icon</label>
-			<input type="file" name="icon">
-		</div>
-		<input type="submit">
+		<input  type="text" name="username" placeholder="User Name" required>
+		<input  type="password" name="password" placeholder="Password" required>
+		<textarea  name = "description" placeholder="Describe youself"></textarea>
+		<label for = "icon">Icon</label>
+		<input type="file" name="icon">
+		<button type="submit" class="btn btn-default">Register</button>
 	</form>
 </div>
 
-
+<link rel="stylesheet" type="text/css" href="./include/css/welcomePage.css">
 <?php require "./include/partials/footer.php" ?>
