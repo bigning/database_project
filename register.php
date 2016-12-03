c2b3151cb7882b11457d6ee60142b045ae8186c4
bigning <bigning1118@gmail.com>
2016-12-03 16:30:02 -0500

    add register.php

--- register.php
+++ register.php
@@ -1,8 +1,30 @@
 <?php
 error_reporting(-1);
+
+$user_name = $_POST["username"];
+$pw = $_POST["password"];
+$profile = $_POST["description"];
+$icon_size = $_FILES["icon"]["size"];
+echo $icon_size;
+if ($icon_size > 0) {
+    $icon_save_name = "./user_icons/" . date('Ymdhms') . "_" . rand();
+    move_uploaded_file($_FILES["icon"]["tmp_name"], $icon_save_name);
+} else {
+    $icon_save_name = null;
+}
+
 $conn = new mysqli("52.203.6.76", "db", "database_project", "recipes");
 if (mysqli_connect_errno()) {
     die("Connection failed: " . $conn->connect_error);
 } 
+
+$query = $conn->prepare("INSERT INTO User(user_name, user_profile, password, user_icon) VALUES (?, ?, ?, ?)");
+if ($icon_size > 0) {
+    $query->bind_param('ssss', $user_name, $profile, $pw, $icon_save_name);
+} else {
+    $query->bind_param('ssss', $user_name, $profile, $pw, $icon_save_name);
+}
+$query->execute();
+
 $conn->close();
 ?>
