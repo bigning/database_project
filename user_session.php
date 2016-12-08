812f77a55a5237c2f0ab5fd8a224c889ba3f1478
bigning <bigning1118@gmail.com>
2016-12-08 13:15:14 -0500

    add query to homepage

---
+++ user_session.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(-1);
+$conn = new mysqli("52.203.6.76", "db", "database_project", "recipes");
+if (mysqli_connect_errno()) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+?>
