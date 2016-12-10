0e29a4244565bcc5959b9b08940e5290da9aa84f
bigning <bigning1118@gmail.com>
2016-12-10 18:08:27 -0500

    create_meeting.php

--- create_meeting.php
+++ create_meeting.php
@@ -10,4 +10,15 @@ if (!array_key_exists("meeting_name", $_GET)) {
 }
 $group_id = $_GET["group_id"];
 $meeting_name = $_GET["meeting_name"];
+$query_str = "INSERT INTO GroupMeeting  (group_id, meeting_name, organiser_id) VALUES (?, ?, ?)";
+if ($query = $conn->prepare($query_str)) {
+    $query->bind_param('isi', $group_id, $meeting_name, $user_id);
+    $query->execute();
+
+    $query->close();
+} else {
+    echo $conn->error;
+}
+header("Location: ./group_detail.php?group_id=$group_id");
+
 ?>
