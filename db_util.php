c473858ab2fd4e526fe54ff0a540c47f7eb59968
BoogieJay <jw4100@nyu.edu>
2016-12-29 22:55:03 -0500

    change database to jie's

--- db_util.php
+++ db_util.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(-1);
-$conn = new mysqli("52.203.6.76", "db", "database_project", "recipes");
+$conn = new mysqli("35.164.15.98", "db", "database_project", "recipes");
 if (mysqli_connect_errno()) {
     die("Connection failed: " . $conn->connect_error);
 } 
